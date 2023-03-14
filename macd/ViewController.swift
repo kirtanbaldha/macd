bcc89b35f9073ce8cc425d363b696f8808e5e234
jay5260 <jaykumbhani4242@gmail.com>
2023-03-14 07:58:53 +0530

    Initial Commit

---
+++ macd/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  macd
+//
+//  Created by R & W on 14/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
